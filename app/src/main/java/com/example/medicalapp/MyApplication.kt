e490d20663d68aa59947d1ed1bab1daeed193510
zhenmeimiaoa <2766167974@qq.com>
2026-04-03 00:11:44 +0800

    Fix: Add missing Activity declarations in AndroidManifest.xml

--- app/src/main/java/com/example/medicalapp/MyApplication.kt
+++ app/src/main/java/com/example/medicalapp/MyApplication.kt
@@ -8,16 +8,11 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         
-        // 设置全局异常捕获
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             Log.e("CRASH", "Uncaught exception in thread " + thread.name, throwable)
             LogActivity.addLog("CRASH", "Thread: " + thread.name)
             LogActivity.addLog("CRASH", "Exception: " + throwable.javaClass.simpleName)
             LogActivity.addLog("CRASH", "Message: " + throwable.message)
-            LogActivity.addLog("CRASH", "Stack: " + Log.getStackTraceString(throwable))
-            
-            // 重新抛出，让系统处理
-            throw throwable
         }
         
         LogActivity.addLog("Application", "App started")
