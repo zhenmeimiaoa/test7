a81fddd36ee555ea1a33b97a9ff99bf6811e66b9
zhenmeimiaoa <2766167974@qq.com>
2026-04-08 10:34:46 +0800

    Fix: Ensure IDCardInfo.kt exists in correct package

---
+++ app/src/main/java/com/example/medicalapp/IDCardInfo.kt
@@ -0,0 +1,9 @@
+﻿package com.example.medicalapp
+
+data class IDCardInfo(
+    val name: String = "",
+    val idNumber: String = "",
+    val gender: String = "",
+    val nation: String = "",
+    val address: String = ""
+)
--- app/src/main/java/com/example/medicalapp/OcrActivity.kt
+++ app/src/main/java/com/example/medicalapp/OcrActivity.kt
@@ -192,3 +192,4 @@ class OcrActivity : AppCompatActivity() {
     }
 }
 
+
